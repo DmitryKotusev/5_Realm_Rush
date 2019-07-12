ada039d459fb9b841282401d49bc9f9a190b09c7
DmitryKotusev <dssa-1966@yandex.ru>
2019-07-13 00:46:44 +0300

    Added enemies.

--- Assets/Scripts/CubeEditor.cs
+++ Assets/Scripts/CubeEditor.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [ExecuteInEditMode]
-public class EditorSnap : MonoBehaviour
+[SelectionBase]
+public class CubeEditor : MonoBehaviour
 {
     [Range(1f, 20f)]
     public float gridSize = 10f;
+
+    TextMesh textMesh;
+
+    private void Start()
+    {
+    }
+
     void Update()
     {
         Vector3 snapPos = Vector3.zero;
@@ -14,5 +22,9 @@ public class EditorSnap : MonoBehaviour
         snapPos.y = 0;
         snapPos.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
         transform.position = snapPos;
+
+        textMesh = GetComponentInChildren<TextMesh>();
+        textMesh.text = transform.position.x / gridSize + ";" + transform.position.z / gridSize;
+        gameObject.name = textMesh.text;
     }
 }
