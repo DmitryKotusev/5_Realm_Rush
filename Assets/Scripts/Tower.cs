089423b0884939d26fa97bc8459fe71856908e6f
DmitryKotusev <dssa-1966@yandex.ru>
2019-07-15 20:38:56 +0300

    Added shooting particles to towers.

--- Assets/Scripts/Tower.cs
+++ Assets/Scripts/Tower.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,9 +9,40 @@ public class Tower : MonoBehaviour
     Transform objectToPan;
     [SerializeField]
     Transform targetEnemy;
+    [SerializeField]
+    float attackRange = 30f;
+    [SerializeField]
+    ParticleSystem projectile;
 
     void Update()
     {
-        objectToPan.LookAt(targetEnemy);
+        if (targetEnemy)
+        {
+            objectToPan.LookAt(targetEnemy);
+            FireAtEnemy();
+        }
+        else
+        {
+            Shoot(false);
+        }
+    }
+
+    private void FireAtEnemy()
+    {
+        float distanceToEnemy = Vector3.Distance(transform.position, targetEnemy.position);
+        if (distanceToEnemy <= attackRange)
+        {
+            Shoot(true);
+        }
+        else
+        {
+            Shoot(false);
+        }
+    }
+
+    private void Shoot(bool doShoot)
+    {
+        var emissionModule = projectile.emission;
+        emissionModule.enabled = doShoot;
     }
 }
